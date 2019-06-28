6a4d22e884cc0ad7d27783e811f16feb292134aa
Antoine Beaupré <anarcat@debian.org>
2019-06-28 12:45:32 -0400

    override keyserver to point to keys.openpgp.org instead of filtering
    As per https://dkg.fifthhorseman.net/blog/openpgp-certificate-flooding.html

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+KEYSERVER="hkps://keys.openpgp.org"
+
 FILTER="cat"
 
 # another filter can be enabled with, for example:
-FILTER="grep -v C4BC2DDB38CCE96485EBE9C2F20691179038E5C6"
+# FILTER="grep -v C4BC2DDB38CCE96485EBE9C2F20691179038E5C6"
 
 while true
 do
@@ -16,7 +18,7 @@ do
 				| sort --random-sort)
 	do
 		echo "I: refreshing $fingerprint..."
-		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$fingerprint" 2> /dev/null
+		gpg --batch --no-tty --no-auto-check-trustdb --keyserver "$KEYSERVER" --refresh-keys "$fingerprint" 2> /dev/null
                 delay=$(( $(tr -cd "[:digit:]" < /dev/urandom | head -c 3) + 300 ))
                 echo "I: sleeping for $delay seconds..."
 		sleep $delay
