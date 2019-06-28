2da3311e8f40602f8797bc47ab2cd4d9979116d9
Antoine Beaupré <anarcat@debian.org>
2019-06-28 14:58:57 -0400

    unbashismify: use od and bc to get more flexible math

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -4,6 +4,13 @@ KEYSERVER="hkps://keys.openpgp.org"
 
 FILTER="cat"
 
+# minimum number of seconds to way
+DELAY_BASE=120
+# a range of random seconds on top of that
+DELAY_RANGE=180
+# maximum is DELAY_BASE + rand(DELAY_RANGE), so anywhere between 2 to
+# 5 minutes by default
+
 # another filter can be enabled with, for example:
 # FILTER="grep -v C4BC2DDB38CCE96485EBE9C2F20691179038E5C6"
 
@@ -19,7 +26,9 @@ do
 	do
 		echo "I: refreshing $fingerprint..."
 		gpg --batch --no-tty --no-auto-check-trustdb --keyserver "$KEYSERVER" --refresh-keys "$fingerprint"
-                delay=$(( $(tr -cd "[:digit:]" < /dev/urandom | head -c 3) + 300 ))
+                # this takes 4 bytes out of urandom and formats them as an unsigned integer
+                r=$(od -vAn -N4 -tu4 < /dev/urandom)
+		delay=$( echo "$DELAY_BASE + $r % $DELAY_RANGE" | bc )
                 echo "I: sleeping for $delay seconds..."
 		sleep $delay
 	done
