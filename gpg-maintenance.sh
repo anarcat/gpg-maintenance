d8dc92d95d5c4df4695b86289d056960ff23a2e6
Antoine Beaupré <anarcat@debian.org>
2019-06-28 11:30:51 -0400

    add an example of how a filter could be implemented

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+FILTER="cat"
+
+# another filter can be enabled with, for example:
+# FILTER="grep -v FINGERPRINT"
+
 while true
 do
 	for fingerprint in $(gpg --list-keys --with-colons --with-fingerprint \
 				| grep --after-context "1" "^pub" \
 				| grep "^fpr" \
 				| cut -d ":" -f "10" \
+				| $FILTER \
 				| sort --random-sort)
 	do
 		sleep $(( $(tr -cd "[:digit:]" < /dev/urandom | head -c 3) + 300 ))
