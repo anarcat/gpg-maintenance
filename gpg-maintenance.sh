b71bacb9d24c7c28d6ae54d917d68eb271eed258
Antoine Beaupré <anarcat@debian.org>
2019-06-28 11:34:55 -0400

    show state

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -7,6 +7,7 @@ FILTER="cat"
 
 while true
 do
+	echo "I: listing all keys..."
 	for fingerprint in $(gpg --list-keys --with-colons --with-fingerprint \
 				| grep --after-context "1" "^pub" \
 				| grep "^fpr" \
@@ -14,7 +15,10 @@ do
 				| $FILTER \
 				| sort --random-sort)
 	do
+		echo "I: refreshing $fingerprint..."
 		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$fingerprint" 2> /dev/null
-		sleep $(( $(tr -cd "[:digit:]" < /dev/urandom | head -c 3) + 300 ))
+                delay=$(( $(tr -cd "[:digit:]" < /dev/urandom | head -c 3) + 300 ))
+                echo "I: sleeping for $delay seconds..."
+		sleep $delay
 	done
 done
